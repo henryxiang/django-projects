5de5d49add189352b7202d6495032dd19126b2f4
Henry Xiang <henryxiang@gmail.com>
2020-11-25 11:20:30 -0800

    updated django-tutorial: created views in Polls app

--- django-tutorial/mysite/polls/views.py
+++ django-tutorial/mysite/polls/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello Django!')
+
+def detail(request, question_id):
+    return HttpResponse(f'question {question_id} details')
+
+def results(request, question_id):
+    return HttpResponse(f'results of question {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'vote on question {question_id}')
